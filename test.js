30d455c42326d0f286cc3c2f056ef73d908aa4b0
Ashley Dawood <ashleymdawood@gmail.com>
2019-03-09 17:26:27 -0700

    adjusted string

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 // this means notes for myself only, wont show up in code
 
-/* this also means writing a comment this is best for multiline comments. */ 
+/* this also means writing a comment this is best for multiline comments. 
+
+shift w q ! is how to get out of VIM editor */ 
 
 
 
-var message = 'Ashley';
+var message = 'Florida is so much better than Arizona'; 
 
 console.log(message);
 
