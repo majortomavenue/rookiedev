d0e6a10d6b21807b81a67c205c351ed80388008f
Ashley Dawood <ashleymdawood@gmail.com>
2019-03-09 17:19:10 -0700

    Hello World

---
+++ test.js
@@ -0,0 +1,10 @@
+// this means notes for myself only, wont show up in code
+
+/* this also means writing a comment this is best for multiline comments. */ 
+
+
+
+var message = 'Ashley';
+
+console.log(message);
+
